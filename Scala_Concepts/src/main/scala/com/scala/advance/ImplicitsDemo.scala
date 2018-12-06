a4840f75790926fb20ac2ba67a155605ea3ed920
Jayvardhan Reddy Vanchireddy <33086172+Jayvardhan-Reddy@users.noreply.github.com>
2018-12-06 16:06:47 -0400

    Implicits and Fold

--- Scala_Concepts/src/main/scala/com/scala/advance/ImplicitsDemo.scala
+++ Scala_Concepts/src/main/scala/com/scala/advance/ImplicitsDemo.scala
@@ -1,5 +1,12 @@
 package com.scala.advance
 
+/* In scala, when we do 1 to 10, It returns a range from 1 to 10. This is an example of implicit type conversion, here "to" method is part
+ *  of RichInt class. Scala compiler converts Int to RichInt to get method definition of "to"
+ * 
+*/
+
 object ImplicitsDemo extends App {
-  
+  implicit
+  val a = 0 to 10
+  println(a)
 }
---
+++ Scala_Concepts/src/main/scala/com/scala/core/FoldLeftRight.scala
@@ -0,0 +1,5 @@
+package com.scala.core
+
+object FoldLeftRight extends App {
+  //https://commitlogs.com/2016/09/10/scala-fold-foldleft-and-foldright/
+}
