072d8ca14b2cd2e86340cc9aa9a90d3bfcc0cb94
Jayvardhan Reddy Vanchireddy <33086172+Jayvardhan-Reddy@users.noreply.github.com>
2019-06-28 15:11:00 -0400

    Scala sequence

---
+++ Scala_Concepts/src/main/scala/com/scala/collection/SequenceDemo.scala
@@ -0,0 +1,11 @@
+package com.scala.collection
+
+/*
+sequence have length
+Elements in sequence can be accessed using Prefix.
+Classes are divided into Seq classes and Buffer classes
+Array and List are considered as Seq classes and they are more frequently used
+*/
+object SequenceDemo extends App {
+  
+}
